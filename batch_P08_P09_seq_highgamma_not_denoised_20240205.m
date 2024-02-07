c37f1e54ea7734adb2130285e0c1331644dc779b
amsmeier <amsmeier@gmail.com>
2024-02-06 21:54:27 -0500

    added info fields into manual artifact detection GUI
    started changing trial cutting script to increase trial window to parameters noted in DBS-SEQ log

---
+++ batch_P08_P09_seq_highgamma_not_denoised_20240205.m
@@ -0,0 +1,26 @@
+%%% run processing steps required to get trialwise high gamma responses from raw fieldtrip data...
+%%% .... from all DBSSEQ subs not-vibration-denoised data
+
+sublist ={...
+     'DM1005';...
+     'DM1007';...
+     'DM1008';...
+     'DM1024';...
+     'DM1025';...
+     'DM1037';...
+     };
+
+addpath('C:\Users\amsmeier\ieeg_ft_funcs_am')
+addpath('C:\Users\amsmeier\ieeg_ft_funcs_am\util')
+
+nsubs = length(sublist);
+for isub = 1:nsubs
+    thissub = sublist{isub}
+    op.sub = thissub;
+
+    close all force
+
+    P08A09_detect_artifact_not_denoised(op)
+    P09_redefine_trial_common_avg_ref_not_denoised(op)
+    P09_compute_high_gamma_trials_not_denoised(op)
+end
