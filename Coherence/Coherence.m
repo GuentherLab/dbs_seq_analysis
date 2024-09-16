ab1d774ee8a98a292a8142c092bbe06bc32e8ec4
girija2020 <82602432+girija2020@users.noreply.github.com>
2024-09-15 22:26:02 -0400

    Added Wavelet Coherence

--- Coherence/Coherence.m
+++ Coherence/Coherence.m
@@ -3,7 +3,7 @@
 % Get the speech onset data. This can be modified to audio or visual onset
 % data depending on the requirements
 % Select start time of window, window duration and the channels
-% input("0 if MSCOHERE, 1 if Wavelet")
+% input("0 if MSCOHERE, 1 if Wavelet") in option
 % Select range of frequencies you want to observe
 
 data = load('/Users/Temporary/Downloads/sub-DM1005_ses-intraop_task-smsl_ft-raw_filt_trial_ar-E_ref-CTAR_not-denoised.mat').D_trial_ref
@@ -12,9 +12,10 @@ start_window = 0
 window_length = 100
 chan_1 = 1
 chan_2 = 120
-option = 0
+option = 1
 freq_lo = 1
-freq_hi = 50
+freq_hi = 70
+voices_per_octave = 12 % specific to wavelet coherence
 
 
 fs = data.fsample
@@ -38,6 +39,32 @@ end
 % Selecting a particular method, either MSCOHERE or Wavelet Coherence
 if option == 1
     % wavelet
+    % We are calculating wavelet coherence for the whole signal and then
+    % taking the necessary range into consideration. this could slow up the
+    % process but this will give us a more fuller curve. depending on time
+    % vs accuracy we could choose to do either of them.
+    k = 1
+    f_ = 0
+    for i = 1:1:length(sp_on)
+        start_time = sp_on(i) - data.time{i}(1) + start_window/1000
+        if isnan(start_time) == 0
+            if isnan(window_length) == 0
+                if start_time*fs+window_length-1 < length(data.time{i})
+                    [wcoh_,wcs_,f_] = wcoherence(data.trial{i}(chan_1, :),data.trial{i}(chan_2, :), fs, FrequencyLimits=[freq_lo,freq_hi],VoicesPerOctave=voices_per_octave)
+                    if i == 1
+                        wcoh_fin = wcoh_(:, int64(start_time*fs):int64(start_time*fs)+window_length)
+                    else
+                        wcoh_fin = wcoh_fin + wcoh_(:, int64(start_time*fs):int64(start_time*fs)+window_length)
+                    end
+                end
+            end
+        end
+    end
+    plot(f_, mean(wcoh_fin,2))
+    xlabel('frequency')
+    ylabel('coherence')
+    title('Coherence vs Frequency Wavelet')
+
 elseif option == 0
     % mscohere
     tp_x = zeros(1,window_length*(num_non_nans))
@@ -59,7 +86,7 @@ elseif option == 0
     plot(f,sqrt(cxy))
     xlabel('frequency')
     ylabel('coherence')
-    title('Coherence vs Frequency')
+    title('Coherence vs Frequency MSCOHERE')
 else 
     % return wrong input
 end
