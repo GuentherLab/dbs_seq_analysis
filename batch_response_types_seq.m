9abf00b63d6d77658f2ea5eae5e16193c0671230
amsmeier <amsmeier@gmail.com>
2026-02-21 03:13:20 -0500

    .

--- batch_response_types_seq.m
+++ batch_response_types_seq.m
@@ -7,17 +7,34 @@ setpaths_dbs_seq()
 % subject_list_filename = [PATH_DATA filesep 'participants.tsv'];
 subject_list_filename = [PATH_DBSSEQ_CODE, filesep, 'dbs_seq_subjects_master.tsv']; 
 
-% resp_signal = 'hg'; ARTIFACT_CRIT = 'E'; 
-resp_signal = 'beta'; ARTIFACT_CRIT = 'F'; 
+resp_signal = 'hg'; ARTIFACT_CRIT = 'E'; rereference_method = 'CMR';
+% resp_signal = 'beta'; ARTIFACT_CRIT = 'F'; % rereference_method = 'none';
 
-rereference_method = 'none';
 % rereference_method = 'CTAR';
-% rereference_method = 'CMR';
 
-subnums = [1005, 1007, 1008, 1024, 1025, 1037];
-%     subnums = [1044, 1048]; 
+
+subnums = [...
+    1005;...
+    1007;...
+    1008;...
+    1024;...
+    1025;...
+    1037;...
+    1044;...
+%     1045;... % resolve switched-reference issue
+    1046;...
+    1047;...
+    1048;...
+%     1049;... % resolve channels table issue
+%     1050;... % finish b06
+%     1051;... % finish b06
+%     1052;... % finish b06    
+%     1054;... % finish b06  
+    ]
+
 
 compiled_responses_filepath = [PATH_RESULTS, filesep, 'resp_all_subjects_', resp_signal, '_ref-',rereference_method]; 
+% compiled_responses_filepath = [PATH_RESULTS, filesep, 'resp_all_44_48_', resp_signal, '_ref-',rereference_method]; 
 
 %% set up sub list
 subnames = arrayfun(@(x)['DM',num2str(x)],subnums','UniformOutput',0);
